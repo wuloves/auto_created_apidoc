98b415c9acd7e056479753c734e4fcfc967807bc
吴陈东 <834318142@qq.com>
2019-11-04 10:51:06 +0800

    苹果机器下自动启动的测试

--- serve.php
+++ serve.php
@@ -1,9 +1,23 @@
 <?php
 
-for ($port = 4000; $port < 10000; $port++) {
-    $hasPrograme = exec('netstat -ano|findstr ' . $port);
-    if (empty($hasPrograme)) {
-        exec('start "" http://127.0.0.1:' . $port);
-        exec('php -S 0.0.0.0:' . $port);
+if (empty($_SERVER)) {
+    for ($port = 4000; $port < 5000; $port++) {
+        $hasPrograme = exec('lsof -i:' . $port);
+        if (empty($hasPrograme)) {
+            exec('start "" http://127.0.0.1:' . $port);
+            exec('php -S 0.0.0.0:' . $port);
+        }
     }
+} else if (strpos($_SERVER['OS'], 'Windows') !== false) {
+    echo '系统类型: ' . $_SERVER['OS'] . PHP_EOL;
+    for ($port = 4000; $port < 10000; $port++) {
+        $hasPrograme = exec('netstat -ano|findstr ' . $port);
+        if (empty($hasPrograme)) {
+            exec('start "" http://127.0.0.1:' . $port);
+            exec('php -S 0.0.0.0:' . $port);
+        }
+    }
+} else {
+    exec('start "" http://127.0.0.1:' . $port);
+    exec('php -S 0.0.0.0:' . $port);
 }
