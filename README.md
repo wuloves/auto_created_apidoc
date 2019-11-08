b12c517dbbc0f91bdfd0b6e76c854298a333fa47
吴陈东 <834318142@qq.com>
2019-11-08 17:52:17 +0800

    解决了mac的错误提示问题

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 自动根据表创建apidoc文档内容
 
-启动服务, windows会自动启动服务并在浏览器中打开相应的地址
+启动服务, windows和mac会自动启动服务并在浏览器中打开相应的地址
 ```
 php serve.php
 ```
 
+auth dev@wuloves.com
--- serve.php
+++ serve.php
@@ -1,6 +1,6 @@
 <?php
 
-if (isset($_SERVER) && strpos($_SERVER['OS'], 'Windows') !== false) {
+if (isset($_SERVER['OS']) && strpos($_SERVER['OS'], 'Windows') !== false) {
     echo '系统类型: ' . $_SERVER['OS'] . PHP_EOL;
     for ($port = 4000; $port < 10000; $port++) {
         $hasPrograme = exec('netstat -ano|findstr ' . $port);
@@ -10,6 +10,7 @@ if (isset($_SERVER) && strpos($_SERVER['OS'], 'Windows') !== false) {
         }
     }
 } else {
+    // mac下
     $port = 4000;
     exec('open "" http://127.0.0.1:' . $port);
     exec('php -S 127.0.0.1:' . $port);
